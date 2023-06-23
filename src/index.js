5fce14c31d8bcd37be427837151064c0346ba6c2
CODEKAPP <117596036+CODEKAPP@users.noreply.github.com>
2023-06-22 22:54:32 -0500

    commit_5.1-00

--- src/index.js
+++ src/index.js
@@ -2,3 +2,53 @@ import axios from 'axios'; // Importa la biblioteca Axios desde npm
 import Notiflix from 'notiflix'; // Importa la biblioteca Notiflix desde npm
 import SimpleLightbox from 'simplelightbox'; // Importa la biblioteca SimpleLightbox desde npm
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Importar las variables de configuración desde el archivo "./js/config.js"
+import { API_URL, API_KEY } from './js/config.js';
+
+// Array para almacenar los eventos
+const arrayEvents = [];
+const arrayEventsName = [];
+const arrayPais = [];
+
+// Petición de eventos
+export function fetchEvents() {
+  fetch(
+    `https://${API_URL}?apikey=${API_KEY}&` +
+      new URLSearchParams({
+        locale: '*',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
+  )
+    .then(response => response.json())
+    .then(data => {
+      // Almacenar los datos en la variable "render"
+      const render = data;
+      console.log(render);
+
+      // Recorrer el objeto "render" para obtener los eventos
+      for (let value in render) {
+        if (arrayEvents.length === 1) {
+          continue;
+        } else {
+          const evento = render._embedded.events;
+          arrayEvents.push(evento);
+        }
+      }
+
+      // Recorrer los eventos para obtener el nombre y agregarlo al array "arrayEventsName"
+      for (let event of arrayEvents[0]) {
+        arrayEventsName.push(event.name);
+        arrayPais.push(event.locale);
+      }
+
+      console.log(arrayEventsName);
+      console.log(arrayPais);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
---
+++ src/js/config.js
@@ -0,0 +1,10 @@
+// Archivo de configuración para almacenar datos privados
+
+// URL de la API
+const API_URL = 'app.ticketmaster.com/discovery/v2/events';
+
+// Clave de API
+const API_KEY = 'lFVCR65Z8sa3CM5tWviQtp768vaQKovb';
+
+// Exportar las variables para poder acceder a ellas desde otros archivos
+export { API_URL, API_KEY };
